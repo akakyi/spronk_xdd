21f2c665ea32fafc1561671f2f4267b7dd0bf71f
Fomin Mikhail <m.fomin@elama.ru>
2023-04-20 23:45:00 +0400

    minor changes in disgusting classes loading -_-

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,8 @@
-import ru.akakyi.spronk.inject.process.collect.ComponentClassAnnotationBeanDescriptionCollector
+import ru.akakyi.spronk.inject.process.collect.ClassAnnotationBeanDescriptionCollector
 import ru.akakyi.spronk.inject.process.utils.PackageClassLoaderUseCase
 
 fun main(args: Array<String>) {
     val loader = PackageClassLoaderUseCase("ru.akakyi.test")
-    val collector = ComponentClassAnnotationBeanDescriptionCollector(loader)
+    val collector = ClassAnnotationBeanDescriptionCollector(loader)
     collector.getDescriptions().forEach(::println)
 }
--- src/main/kotlin/ru/akakyi/spronk/inject/process/collect/ClassAnnotationBeanDescriptionCollector.kt
+++ src/main/kotlin/ru/akakyi/spronk/inject/process/collect/ClassAnnotationBeanDescriptionCollector.kt
@@ -1,40 +1,61 @@
 package ru.akakyi.spronk.inject.process.collect
 
+import ru.akakyi.spronk.inject.annotations.Component
 import ru.akakyi.spronk.inject.annotations.PutIn
 import ru.akakyi.spronk.inject.process.BeanDescription
 import ru.akakyi.spronk.inject.process.utils.CustomClassLoader
 import kotlin.reflect.KClass
+import kotlin.reflect.KFunction
 
-abstract class ClassAnnotationBeanDescriptionCollector(
+class ClassAnnotationBeanDescriptionCollector(
     private val packageClassLoaderUseCase: CustomClassLoader
 ) : BeanDescriptionCollector {
+
+    companion object {
+
+        val COMPONENT_ANNOTATION_CLASS = Component::class
+
+        val INJECT_ANNOTATION_CLASS = PutIn::class
+
+    }
 
     override fun getDescriptions(): Set<BeanDescription> {
         val classes = packageClassLoaderUseCase.execute()
 
-        return classes.filter { klass ->
-            klass.annotations.any { annotation ->
-                targetBeanClassAnnotations.any { it.isInstance(annotation) }
-            }
-        }.mapNotNull { klass ->
-            klass.simpleName?.let {
-                it to klass.constructors.first()
+        return classes.mapNotNull { klass ->
+            val annotation = klass.annotations.firstOrNull { annotation ->
+                COMPONENT_ANNOTATION_CLASS.isInstance(annotation)
+            }?.let { COMPONENT_ANNOTATION_CLASS.javaObjectType.cast(it) }
+            annotation?.let { klass to annotation }
+        }.mapNotNull { klassAndAnnotation ->
+            klassAndAnnotation.first.qualifiedName?.let {
+                PreDescription(
+                    className = it,
+                    beanName = klassAndAnnotation.second.name,
+                    //TODO как вытащить праймари конструктор?
+                    constructor = klassAndAnnotation.first.constructors.first()
+                )
             }
         }.map { nameWithConstr ->
-            val argsNames = nameWithConstr.second.parameters
+            val argsNames = nameWithConstr.constructor.parameters
                 .map { param ->
-                    param.annotations.first { it == PutIn::class } as PutIn
+                    param.annotations.first { it == INJECT_ANNOTATION_CLASS::class }
                 }.map {
-                    it.name
+                    val annotation = INJECT_ANNOTATION_CLASS.javaObjectType.cast(it)
+                    annotation.name
                 }
             BeanDescription(
-                classFullName = ,
-                name = nameWithConstr.first,
+                classFullName = nameWithConstr.className,
+                name = nameWithConstr.beanName,
                 dependenciesNames = argsNames
             )
         }.toSet()
     }
 
-    protected abstract val targetBeanClassAnnotations: Set<KClass<out Annotation>>
+    private data class PreDescription(
+        val className: String,
+        val beanName: String,
+        val constructor: KFunction<*>
+    )
 
 }
