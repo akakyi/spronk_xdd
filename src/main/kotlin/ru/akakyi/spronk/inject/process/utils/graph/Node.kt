319ffdafd5c162a0f7bd82878f50ae888f6037d4
Fomin Mikhail <m.fomin@elama.ru>
2023-04-23 23:46:44 +0400

    Нахрен дерево. Поиск n^2, в каждую итерацию ищем бины, чьи зависимости уже инстанциированы (это план на некст коммит, ща лень)

--- src/main/kotlin/ru/akakyi/spronk/inject/process/utils/graph/Node.kt
+++
@@ -1,8 +0,0 @@
-package ru.akakyi.spronk.inject.process.utils.graph
-
-data class Node<Type : Any>(
-    val value: Type,
-    val parent: Node<Type>?,
-    val childs: MutableList<Node<Type>>,
-    val level: Int
-)
